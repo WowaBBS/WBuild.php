7c86fb414b95d41c7e03116a0a52ab0ba5ba50aa
Vladimir Savin <wowabbs@gmail.com>
2018-07-10 04:27:40 +0400

    Added CPP Analyzer

---
+++ Src/Builder/Compiler/CPP/Analyzer/Test.h
@@ -0,0 +1,20 @@
+
+// comment next line \
+#include "Hello"
+
+#include "Main/Application.h"
+#include <Main/Application.h>
+//#include "PersMixer.h"
+//#include "ModelInfo.h"
+//#include "WorldBuilder.h"
+
+CRLF
+
+/*
+ #include "ModelInfo.h"
+ //#include "ModelInfo.h"
+ //TODO: #include "ToDo.h"
+ *#include "ModelInfo.h"
+*/
+
+End line
