24ba5a9a5cab2b3812987aaa9ff9fba2f4af0238
Vladimir Savin <wowabbs@gmail.com>
2018-10-09 05:38:33 +0400

    Fix spaces

--- Src/Using.php
+++ Src/Using.php
@@ -1,8 +1,8 @@
 <?php
- if(file_exists(DirName(__FILE__).'/WLib/Using.php'))
-   Include_Once('WLib/Using.php');
- else
-   return Include_Once '_Using.php';
- 
- $Loader->AddSearchPath(DirName(__FILE__)); 
+  if(file_exists(DirName(__FILE__).'/WLib/Using.php'))
+    Include_Once('WLib/Using.php');
+  else
+    return Include_Once '_Using.php';
+  
+  $Loader->AddSearchPath(DirName(__FILE__)); 
 ?>
