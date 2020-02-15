2d55517ff839d9dada5cd68419e682dcff1dac8c
Wowa <wowabbs@gmail.com>
2020-02-16 03:34:50 +0400

    Fix Cpp analyzer

--- Src/Builder/Compiler/CPP/Analyzer/Test.h
+++ Src/Builder/Compiler/CPP/Analyzer/Test.h
@@ -2,14 +2,24 @@
 // comment next line \
 #include "Hello"
 
+#define AAA \
+  #include "Define"
+
 #include "Main/Application.h"
 #include <Main/Application.h>
 char *Str="#include <Exclude.h>"
+char *Str2="\
+#include <String>"  #include <AfterString>
+char *Str3=R"AAA(\
+#include <RawString>
+)AAA" #include <AfterRawString>
 //#include "PersMixer.h"
 //#include "ModelInfo.h"
 //#include "WorldBuilder.h"
 
 //public  ref class TestFormControl : public System::Windows::Forms::Form
+
+const v=10'000;
 
 CRLF
 
